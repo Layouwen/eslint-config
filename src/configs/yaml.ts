ca2d7eeb1b1dfcf992dddf98441d0b152db6e972
Avan <layouwen@gmail.com>
2025-01-08 09:06:56 +0800

    fix: eslint error

--- src/configs/yaml.ts
+++ src/configs/yaml.ts
@@ -60,7 +60,7 @@ export async function yaml(
               'yaml/indent': ['error', indent === 'tab' ? 2 : indent],
               'yaml/key-spacing': 'error',
               'yaml/no-tab-indent': 'error',
-              'yaml/quotes': ['error', { avoidEscape: false, prefer: quotes }],
+              'yaml/quotes': ['error', { avoidEscape: false, prefer: quotes === 'single' ? 'single' : 'double' }],
               'yaml/spaced-comment': 'error',
             }
           : {},
